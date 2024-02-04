8b0f9f00dced9733fbdf8834cdc59fd431f84ba3
thequantman <137754020+thequantman@users.noreply.github.com>
2024-02-04 11:08:59 +0000

    Add files via upload
    modified Adventure.hs which now works with haskeline

--- Adventure.hs
+++ Adventure.hs
@@ -1,18 +1,13 @@
 module Main where
 
-import World
-import Actions
+import World    -- Contains the game world's data structures and logic.
+import Actions  -- Includes definitions for game actions and helper functions.
+import Control.Monad.Trans (liftIO)
+import System.Console.Haskeline
 
-import Control.Monad
-import System.IO
-import System.Exit
+winmessage = "Congratulations, you have made it out of the house.\nNow go to your lectures..."
 
-winmessage = "Congratulations, you have made it out of the house.\n" ++
-             "Now go to your lectures..."
-
-{- Given a game state, and user input (as a list of words) return a 
-   new game state and a message for the user. -}
-
+-- Given a game state, and user input (as a list of words) return a new game state and a message for the user.
 process :: GameData -> [String] -> (GameData, String)
 process state [cmd,arg] = case actions cmd of
                             Just fn -> fn arg state
@@ -22,20 +17,25 @@ process state [cmd]     = case commands cmd of
                             Nothing -> (state, "I don't understand")
 process state _ = (state, "I don't understand")
 
-repl :: GameData -> IO GameData
+-- REPL function to interact with the game, showing available actions and handling user input with Haskeline.
+repl :: GameData -> InputT IO GameData
 repl state | finished state = return state
-repl state = do putStr "\n"
-                print state
-                putStr "What now? "
-                hFlush stdout
-                cmd <- getLine
-                let (state', msg) = process state (words cmd)
-                putStr "\n"
-                putStrLn msg
-                if (won state') then do putStrLn winmessage
-                                        return state'
-                               else repl state'
+repl state = do
+  outputStrLn "" -- Equivalent to `putStr "\n"`
+  liftIO $ print state
+  outputStr "What now? "
+  minput <- getInputLine ""
+  case minput of
+    Nothing -> return state -- Handle EOF or CTRL+D
+    Just cmd -> do
+      let (state', msg) = process state (words cmd)
+      outputStrLn "" -- New line for clean separation
+      outputStrLn msg
+      if won state'
+        then do outputStrLn winmessage
+                return state'
+        else repl state'
 
+-- Main function to start the game loop.
 main :: IO ()
-main = do repl initState
-          return ()
+main = runInputT defaultSettings (repl initState) >> return ()
